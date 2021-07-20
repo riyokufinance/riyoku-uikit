913e220b48cc31c19d3e1e3ca2229f534206bcb5
Literally Collective <bluedragon.cstar@outlook.com>
2021-07-20 14:33:00 -0300

    Feat: update color

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2776,9 +2776,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primary: "#CEFED8",
+    primaryBright: "#CEFED8",
+    primaryDark: "#CEFED8",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
